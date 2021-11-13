d4baa5661d3f9fe970656cd085e7449bf131d507
Johnny Wu <johnnyw131@gmail.com>
2021-11-12 23:49:39 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.querySelector('#main')
+
+main.remove();
+
+var newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id","victory")
+
+newHeader.innerHTML = "Johnny is the champion"
+document.body.append(newHeader);
+
+
+
